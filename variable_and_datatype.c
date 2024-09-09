7bd4ae4a6c4f98561fff6170cf8249cee723e283
Suraj Sahani <93707688+engineersurajsahani@users.noreply.github.com>
2024-09-09 10:56:40 +0530

    Create variable_and_datatype.c

---
+++ variable_and_datatype.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // Variable :- It is like a container in which we can store some value.
+    
+    // Data Type :- It define the type of the value or data which are going to be stored in the variable.
+    
+    int a=5; // integer literal --> %d
+    float f=5.5;// floating point literal --> %f 
+    char c='s';// character literal --> %c 
+    
+    printf("%d",a);
+    printf("\n%.0f",f);
+    printf("\n%c",c);
+    
+    printf("\n\n%d  %f %c",a,f,c);
+
+    return 0;
+}
