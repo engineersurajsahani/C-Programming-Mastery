bad55e6703e92b34b5dc51fe46d0f6699faeb422
Suraj Sahani <93707688+engineersurajsahani@users.noreply.github.com>
2024-09-11 12:20:35 +0530

    Create switch_statements.c

---
+++ switch_statements.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    // Conditional Statements 
+    
+    // if 
+    // if else 
+    // if else if 
+    //  switch case 
+    
+    int day=55;
+    
+    switch(day){
+        
+        case 1:
+        printf("\nMonday");
+        break;
+        
+        case 2:
+        printf("\nTuesday");
+        break;
+        
+        case 3:
+        printf("\nWednesday");
+        break;
+        
+        case 4:
+        printf("\nThursday");
+        break;
+        
+        case 5:
+        printf("\nFriday");
+        break;
+        
+        case 6:
+        printf("\nSaturday");
+        break;
+        
+        case 7:
+        printf("\nSunday");
+        break;
+        
+        default:
+        printf("\nNot Know");
+    }
+
+    return 0;
+}
