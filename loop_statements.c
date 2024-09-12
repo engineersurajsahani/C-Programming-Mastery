b65ccc508f2c88ce5a2a8dea4e83ba9481a12146
Suraj Sahani <93707688+engineersurajsahani@users.noreply.github.com>
2024-09-12 11:03:08 +0530

    Create loop_statements.c

---
+++ loop_statements.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("C Programming By Elevate Coders");
+    
+    // Loop Statements 
+    
+    // for loop 
+    //  while loop 
+    //  do while loop 
+    
+    for(int i=1;i<11;i++){
+      printf("\nC Programming");
+    }
+    
+    printf("\n\n*************************************\n");
+    
+    int i=1;
+    while(i<11){
+      printf("\nC Programming");
+      i++;
+    }
+    
+    printf("\n\n*************************************\n");
+    
+   i=1;
+    do{
+      printf("\nC Programming");
+      i++;
+    }while(i<11);
+    
+    
+    
+    
+    
+}
