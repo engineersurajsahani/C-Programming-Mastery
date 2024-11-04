bcf8631dece5bc185152bac3738b7c843f57067d
Suraj Sahani <93707688+engineersurajsahani@users.noreply.github.com>
2024-11-04 09:19:21 +0530

    Create call_by_pointer.c

---
+++ call_by_pointer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void swap(int *a,int *b){
+    
+    printf("\nThe Address of a :- %d",a);
+    printf("\nThe Address of b :- %d",b);
+    
+    int c=*a;
+    *a=*b;
+    *b=c;
+}
+
+int main()
+{
+    
+    // Call By Pointer or Call By Address
+    
+    int a=10;
+    
+    int b=20;
+    
+    printf("\nThe Value of a :- %d",a);
+    printf("\nThe Value of b :- %d",b);
+    
+    printf("\nThe Address of a :- %d",&a);
+    printf("\nThe Address of b :- %d",&b);
+    
+    swap(&a,&b);
+    
+    printf("\nThe Value of a :- %d",a);
+    printf("\nThe Value of b :- %d",b);
+    
+    return 0;
+}
